1c9b3930ad6ccc036c55acd4bee4ad01ece6fc02
Dannyw07 <106722381+Dannyw07@users.noreply.github.com>
2023-01-28 02:26:27 +0800

    updated serverjs

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -1,22 +1,21 @@
 const { comment } = require("../models");
 
-const addNews = async (req, res) => {
-    const id = req.params.id;
-    try {
-      // let data = {
-      //   booking_id: id,
-      //   reference_date: req.body.reference_date,
-      //   booking_date: req.body.booking_date,
-      //   booking_time: req.body.booking_time,
-      //   price: req.body.price,
-      };
-      const timeSlot = await comment.create(data);
-      res.status(200).send(timeSlot);
-    } catch (err) {
-      res.json(err);
-    }
-  };
+// const addNews = async (req, res) => {
+//     const id = req.params.id;
+//     try {
+//       // let data = {
+//       //   booking_id: id,
+//       //   reference_date: req.body.reference_date,
+//       //   booking_date: req.body.booking_date,
+//       //   booking_time: req.body.booking_time,
+//       //   price: req.body.price,
+//       };
+//       const timeSlot = await comment.create(data);
+//       res.status(200).send(timeSlot);
+//     } catch (err) {
+//       res.json(err);
+//     }
+//   };
 module.exports = {
-    addNews,
-    
-  };
+  addNews,
+};
--- server/controllers/golfClubController.js
+++ server/controllers/golfClubController.js
@@ -1,14 +1,45 @@
 //create main Model
 
 const { golfclub } = require("../models");
+const cloudinary = require("../utils/cloudinary");
 
 //create golfclub
 
 const addGolfCourse = async (req, res) => {
-  const golfClub = req.body;
+  // const golfClub = req.body;
+  const {
+    gc_name,
+    gc_type,
+    gc_city,
+    gc_address,
+    gc_distance,
+    gc_photos,
+    gc_title,
+    gc_desc,
+    gc_price,
+  } = req.body;
   try {
-    await golfclub.create(golfClub);
-    res.json(golfClub);
+    if (gc_photos) {
+      const uploadRes = await cloudinary.uploader.upload(gc_photos, {
+        upload_preset: "golfClub",
+      });
+
+      if (uploadRes) {
+        golfclub.create({
+          gc_name: gc_name,
+          gc_type: gc_type,
+          gc_city: gc_city,
+          gc_address: gc_address,
+          gc_distance: gc_distance,
+          gc_title: gc_title,
+          gc_desc: gc_desc,
+          gc_price: gc_price,
+        });
+        res.json("golf club creation completed");
+      }
+    }
+    // await golfclub.create(golfClub);
+    // res.json(golfClub);
   } catch (err) {
     res.json(err);
   }
